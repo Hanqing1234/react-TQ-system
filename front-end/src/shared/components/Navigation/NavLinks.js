2917b496e861ca52e20ddb34751e3273f65b2701
Hanqing Liu <lhq3361951@gmail.com>
2022-03-16 09:44:08 -0400

    initialized front-end and create some shared UI components

---
+++ front-end/src/shared/components/Navigation/NavLinks.js
@@ -0,0 +1,42 @@
+import { useContext } from "react";
+import { NavLink } from "react-router-dom";
+
+import { AuthContext } from "../../context/auth-context";
+
+import "./NavLinks.css";
+
+const NavLinks = (props) => {
+  const auth = useContext(AuthContext);
+
+  return (
+    <ul className="nav-links">
+      <li>
+        <NavLink to="/" exact>
+          All Users
+        </NavLink>
+      </li>
+      {auth.isLoggedIn && (
+        <li>
+          <NavLink to={`/${auth.userId}/places`}>My Places</NavLink>
+        </li>
+      )}
+      {auth.isLoggedIn && (
+        <li>
+          <NavLink to="/places/new">New Place</NavLink>
+        </li>
+      )}
+      {!auth.isLoggedIn && (
+        <li>
+          <NavLink to="/auth">Authenticate</NavLink>
+        </li>
+      )}
+      {auth.isLoggedIn && (
+        <li>
+          <button onClick={auth.logout}>LOGOUT</button>
+        </li>
+      )}
+    </ul>
+  );
+};
+
+export default NavLinks;
