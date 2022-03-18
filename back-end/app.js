82239a7d3f6d2bc1a30dd8fd7c534b68cab5151e
Freya <64510536+FreyaKim0@users.noreply.github.com>
2022-03-18 18:14:33 -0400

    rebase main branch

--- back-end/app.js
+++ back-end/app.js
@@ -49,7 +49,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@hanqing.xas3b.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+    `mongodb+srv://hanqing:lhq3361951@hanqing.xas3b.mongodb.net/mern?retryWrites=true&w=majority`
   )
   .then(() => {
     app.listen(process.env.PORT || 5000);
