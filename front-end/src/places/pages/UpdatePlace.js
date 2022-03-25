c62fbfcfd89f8d4e80efc153ccc43a71c5fde522
Elvis Li <presley.us@gmail.com>
2022-03-25 18:51:45 -0400

    Remove uselesscode on UpdatePlace page

--- front-end/src/places/pages/UpdatePlace.js
+++ front-end/src/places/pages/UpdatePlace.js
@@ -16,13 +16,11 @@ import "./PlaceForm.css";
 import Radio from "@mui/material/Radio";
 import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
-import FormControl from "@mui/material/FormControl";
 import FormLabel from "@mui/material/FormLabel";
 
 import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
 
 const UpdatePlace = () => {
-  const auth = useContext(AuthContext);
   const placeId = useParams().placeId;
   const history = useHistory();
 
